3cca6d1a464aa4e256b0f6228abb06c0ef300ed0
fam.lam@live.nl <fam.lam@live.nl@28ff3176-616b-cce0-e209-ffee7925d7bd>
2011-06-07 18:55:29 +0000

    Version 1.3.0 -Better compression of mailto.js -Display WLM and Hotmail as two different items, although they are the same -Support BCC for Gmail and Yahoo mail -Support Zoho mail

--- mailto - uncompressed.js
+++ mailto - uncompressed.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function() {
-  var i, mailtoLinks = [], allHref = document.querySelectorAll("[href], [HREF]");
+  var i;
+  var mailtoLinks = [];
+  var allHref = document.querySelectorAll("[href], [HREF]");
   for (i=0; i<allHref.length; i++) {
     if (/^mailto\:.+/i.test(allHref[i].href))
       mailtoLinks.push(allHref[i]);
@@ -8,7 +10,8 @@ window.addEventListener("load", function() {
 
   if (mailtoLinks.length) {
     chrome.extension.sendRequest("email", function(email) {
-      var mailto, i;
+      var mailto;
+      var i;
       for (mailto = 0; mailto < mailtoLinks.length; mailto++) {
         var match = mailtoLinks[mailto].href.match(/^mailto\:(.+)$/i);
         if (!match) continue;
@@ -27,7 +30,9 @@ window.addEventListener("load", function() {
 
         var link = "";
         switch (email) {
+          case "wlm":
           case "hotmail":
+            // to cc subject body
             var hotmaillink = "compose?To=";
             hotmaillink += (queryparts.to || "");
             hotmaillink += (queryparts.cc ? "&CC=" + queryparts.cc : "");
@@ -36,18 +41,27 @@ window.addEventListener("load", function() {
             link = "http://mail.live.com/?rru=" + escape(hotmaillink);
             break;
           case "gmail":
+            // to cc bcc subject body
             var gmaillink = (queryparts.to || "");
             gmaillink += (queryparts.cc ? "&cc=" + queryparts.cc : "");
+            gmaillink += (queryparts.bcc ? "&bcc=" + queryparts.bcc : "");
             gmaillink += (queryparts.subject ? "&su=" + queryparts.subject : "");
             gmaillink += (queryparts.body ? "&body=" + queryparts.body : "");
             link = "https://mail.google.com/mail/?view=cm&tf=1&to=" + gmaillink;
             break;
           case "ymail":
+            // to cc bcc subject body
             var ymaillink = (queryparts.to || "");
             ymaillink += (queryparts.cc ? "&Cc=" + queryparts.cc : "");
+            ymaillink += (queryparts.bcc ? "&Bcc=" + queryparts.bcc : "");
             ymaillink += (queryparts.subject ? "&Subject=" + queryparts.subject : "");
             ymaillink += (queryparts.body ? "&Body=" + queryparts.body : "");
             link = "http://compose.mail.yahoo.com/?To=" + ymaillink;
+            break;
+          case "zoho":
+            // to
+            var zoholink = (queryparts.to || "");
+            link = "https://zmail.zoho.com/mail/compose.do?extsrc=mailto&mode=compose&tp=zb&ct=" + zoholink;
             break;
         }
 
