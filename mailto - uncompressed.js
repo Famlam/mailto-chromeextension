ab8ffc8f7d6fe3d151c52ef9324fa9d5233d2135
fam.lam@live.nl <fam.lam@live.nl@28ff3176-616b-cce0-e209-ffee7925d7bd>
2011-07-09 13:58:14 +0000

    Version 1.10.0 -Mailto:// links were incorrectly parsed as 'to=//' -Make it work for links inserted after the page has loaded

--- mailto - uncompressed.js
+++ mailto - uncompressed.js
@@ -1,109 +1,93 @@
-window.addEventListener("load", function() {
+var a = function(e) {
   var i;
-  var mailtoLinks = [];
-  var allHref = document.querySelectorAll("[href], form[action]");
-  for (i=0; i<allHref.length; i++) {
-    if (/^mailto\:/i.test(allHref[i].href || allHref[i].action)) {
-      mailtoLinks.push(allHref[i]);
+  var mailtoLink = "";
+  var target = e.target;
+  if (e.type === "submit") {
+    mailtoLink = target.action.replace(/\?.*$/,"");
+    for (i=0; i<target.length; i++) {
+      if (target[i].name && target[i].value) {
+        mailtoLink += "&" + window.escape(target[i].name) + "=" + window.escape(target[i].value);
+      }
     }
+  } else {
+    while (!target.href && target.parentNode) {
+      target = target.parentNode;
+    }
+    mailtoLink = target.href;
   }
-  window.removeEventListener("load", arguments.callee);
+  if (!/^mailto\:/i.test(mailtoLink || "")) {
+    return;
+  }
 
-  if (mailtoLinks.length) {
-    var openMailtoLink = function(e) {
-      chrome.extension.sendRequest("email", function(email) {
-        var i;
-        var mailtoLink = "";
-        var target = e.target;
-        if (target.nodeName === "FORM") {
-          mailtoLink = target.action.replace(/\?.*$/,"");
-          for (i=0; i<target.length; i++) {
-            if (target[i].name && target[i].value) {
-              mailtoLink += "&" + window.escape(target[i].name) + "=" + window.escape(target[i].value);
-            }
-          }
-        } else {
-          while (!target.href && target.parentNode) {
-            target = target.parentNode;
-          }
-          mailtoLink = target.href;
+  chrome.extension.sendRequest("email", function(email) {
+    var queryparts = {};
+    var params = ("to=" + mailtoLink.replace(/mailto\:(\/\/)?/i, '')).replace('?', '&').split('&');
+    for (i = 0; i < params.length; i++) {
+      var split = params[i].split('=');
+      var what = split[0].toLowerCase();
+      if (queryparts[what]) {
+        if (split[1] && (what === "to" || what === "bcc" || what === "cc")) {
+          split[1] = queryparts[what] + ", " + split[1];
+        } else if (what === "body") {
+          split[1] = queryparts[what] + "%0D%0A" + split[1];
         }
-        if (!/^mailto\:/i.test(mailtoLink || "")) {
-          return;
-        }
+      }
+      if (split[1]) {
+        queryparts[what] = window.unescape(split[1]);
+      }
+    }
 
-        var queryparts = {};
-        var params = ("to=" + mailtoLink.substr(7)).replace(/\?/,'&').split('&');
-        for (i = 0; i < params.length; i++) {
-          var split = params[i].split('=');
-          var what = split[0].toLowerCase();
-          if (queryparts[what]) {
-            if (split[1] && (what === "to" || what === "bcc" || what === "cc")) {
-              split[1] = queryparts[what] + ", " + split[1];
-            } else if (what === "body") {
-              split[1] = queryparts[what] + "%0D%0A" + split[1];
-            }
-          }
-          if (split[1]) {
-            queryparts[what] = window.unescape(split[1]);
+    var createPart = function(part, prefix, isAddress, useSemicolon) {
+      var result = "";
+      if (part && prefix !== undefined) {
+        result = part;
+        if (isAddress) {
+          result = result.replace(/(^|\,)[^\,]*\<(.+?\@.+?\..+?)\>/g, "$1$2");
+          if (useSemicolon) {
+            result = result.replace(/\,/g, ';');
           }
         }
-
-        var createPart = function(part, prefix, isAddress, useSemicolon) {
-          var result = "";
-          if (part && prefix !== undefined) {
-            result = part;
-            if (isAddress) {
-              result = result.replace(/(^|\,)[^\,]*\<(.+?\@.+?\..+?)\>/g, "$1$2");
-              if (useSemicolon) {
-                result = result.replace(/\,/g, ';');
-              }
-            }
-            result = prefix + window.escape(result);
-          }
-          return result;
-        };
-        var openLink = function(base, parts, prefixCC, prefixBCC, prefixSubject, prefixBody, useSemicolonAndEscape) {
-          var link = createPart(parts.to, "", true, useSemicolonAndEscape) +
-            createPart(parts.cc, prefixCC, true, useSemicolonAndEscape) +
-            createPart(parts.bcc, prefixBCC, true) +
-            createPart(parts.subject, prefixSubject) +
-            createPart(parts.body, prefixBody);
-          if (useSemicolonAndEscape) {
-            link = window.escape(link);
-          }
-          window.open(base + link);
-        };
-
-        switch (email) {
-          case "wlm":
-          case "hotmail":
-            // to cc subject body
-            openLink("http://mail.live.com/?rru=compose%3FTo%3D", queryparts, "&CC=", undefined, "&subject=", "&body=", true);
-            break;
-          case "gmail":
-            // to cc bcc subject body
-            openLink("https://mail.google.com/mail/?view=cm&tf=1&to=", queryparts, "&cc=", "&bcc=", "&su=", "&body=");
-            break;
-          case "ymail":
-            // to cc bcc subject body
-            openLink("http://compose.mail.yahoo.com/?To=", queryparts, "&Cc=", "&Bcc=", "&Subj=", "&Body=");
-            break;
-          case "zoho":
-            // to
-            openLink("https://zmail.zoho.com/mail/compose.do?extsrc=mailto&mode=compose&tp=zb&ct=", queryparts);
-            break;
-          case "fastmail":
-            // to cc bcc subject body
-            openLink("http://ssl.fastmail.fm/action/compose/?to=", queryparts, "&cc=", "&bcc=", "&subject=", "&body=");
-            break;
-        }
-      });
-      e.preventDefault();
+        result = prefix + window.escape(result);
+      }
+      return result;
+    };
+    var openLink = function(base, prefixCC, prefixBCC, prefixSubject, prefixBody, useSemicolonAndEscape) {
+      var link = createPart(queryparts.to, "", true, useSemicolonAndEscape) +
+        createPart(queryparts.cc, prefixCC, true, useSemicolonAndEscape) +
+        createPart(queryparts.bcc, prefixBCC, true) +
+        createPart(queryparts.subject, prefixSubject) +
+        createPart(queryparts.body, prefixBody);
+      if (useSemicolonAndEscape) {
+        link = window.escape(link);
+      }
+      window.open(base + link);
     };
 
-    for (i = 0; i < mailtoLinks.length; i++) {
-      mailtoLinks[i].addEventListener(mailtoLinks[i].nodeName === "FORM" ? "submit" : "click", openMailtoLink);
+    switch (email) {
+      case "wlm":
+      case "hotmail":
+        // to cc subject body
+        openLink("http://mail.live.com/?rru=compose%3FTo%3D", "&CC=", undefined, "&subject=", "&body=", true);
+        break;
+      case "gmail":
+        // to cc bcc subject body
+        openLink("https://mail.google.com/mail/?view=cm&tf=1&to=", "&cc=", "&bcc=", "&su=", "&body=");
+        break;
+      case "ymail":
+        // to cc bcc subject body
+        openLink("http://compose.mail.yahoo.com/?To=", "&Cc=", "&Bcc=", "&Subj=", "&Body=");
+        break;
+      case "zoho":
+        // to
+        openLink("https://zmail.zoho.com/mail/compose.do?extsrc=mailto&mode=compose&tp=zb&ct=");
+        break;
+      case "fastmail":
+        // to cc bcc subject body
+        openLink("http://ssl.fastmail.fm/action/compose/?to=", "&cc=", "&bcc=", "&subject=", "&body=");
+        break;
     }
-  }
-});
+  });
+  e.preventDefault();
+};
+document.addEventListener("submit", a);
+document.addEventListener("click", a);
