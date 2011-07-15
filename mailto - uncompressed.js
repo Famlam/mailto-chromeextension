c71d99919d80803b15e200e07301679c3fd51af1
fam.lam@live.nl <fam.lam@live.nl@28ff3176-616b-cce0-e209-ffee7925d7bd>
2011-07-15 15:55:25 +0000

    Version 1.11.0 Preparing code for custom URLs

--- mailto - uncompressed.js
+++ mailto - uncompressed.js
@@ -19,73 +19,10 @@ var a = function(e) {
   if (!mailtoLink || !regex.test(mailtoLink)) {
     return;
   }
-
-  chrome.extension.sendRequest("email", function(email) {
-    var queryparts = {};
-    var params = ("to=" + mailtoLink.replace(regex, '')).replace('?', '&').split('&');
-    for (i = 0; i < params.length; i++) {
-      var split = params[i].split('=');
-      var what = split[0].toLowerCase();
-      if (queryparts[what]) {
-        if (split[1] && (what === "to" || what === "bcc" || what === "cc")) {
-          split[1] = queryparts[what] + ", " + split[1];
-        } else if (what === "body") {
-          split[1] = queryparts[what] + "%0D%0A" + split[1];
-        }
-      }
-      if (split[1]) {
-        queryparts[what] = window.unescape(split[1]);
-      }
-    }
-
-    var createPart = function(part, prefix, isAddress, useSemicolon) {
-      var result = "";
-      if (part && prefix !== undefined) {
-        result = part;
-        if (isAddress) {
-          result = result.replace(/(^|\,)[^\,]*\<(.+?\@.+?\..+?)\>/g, "$1$2");
-          if (useSemicolon) {
-            result = result.replace(/\,/g, ';');
-          }
-        }
-        result = prefix + window.escape(result);
-      }
-      return result;
-    };
-    var openLink = function(base, prefixCC, prefixBCC, prefixSubject, prefixBody, useSemicolonAndEscape) {
-      var link = createPart(queryparts.to, "", true, useSemicolonAndEscape) +
-        createPart(queryparts.cc, prefixCC, true, useSemicolonAndEscape) +
-        createPart(queryparts.bcc, prefixBCC, true) +
-        createPart(queryparts.subject, prefixSubject) +
-        createPart(queryparts.body, prefixBody);
-      if (useSemicolonAndEscape) {
-        link = window.escape(link);
-      }
-      window.open(base + link);
-    };
-
-    switch (email) {
-      case "wlm":
-      case "hotmail":
-        // to cc subject body
-        openLink("http://mail.live.com/?rru=compose%3FTo%3D", "&CC=", undefined, "&subject=", "&body=", true);
-        break;
-      case "gmail":
-        // to cc bcc subject body
-        openLink("https://mail.google.com/mail/?view=cm&tf=1&to=", "&cc=", "&bcc=", "&su=", "&body=");
-        break;
-      case "ymail":
-        // to cc bcc subject body
-        openLink("http://compose.mail.yahoo.com/?To=", "&Cc=", "&Bcc=", "&Subj=", "&Body=");
-        break;
-      case "zoho":
-        // to
-        openLink("https://zmail.zoho.com/mail/compose.do?extsrc=mailto&mode=compose&tp=zb&ct=");
-        break;
-      case "fastmail":
-        // to cc bcc subject body
-        openLink("http://ssl.fastmail.fm/action/compose/?to=", "&cc=", "&bcc=", "&subject=", "&body=");
-        break;
+  mailtoLink = mailtoLink.replace(regex, '');
+  chrome.extension.sendRequest(mailtoLink, function(link) {
+    if (link) {
+      window.open(link);
     }
   });
   e.preventDefault();
